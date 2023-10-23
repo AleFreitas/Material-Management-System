435f6f3a6c4b23f7b1ffa0ad3a2d9bb7c2335718
Alexandre de Freitas Souza <alexandre.souza@pixforce.ai>
2023-10-23 08:10:57 -0300

    refactor: server online message refactored

--- src/app.ts
+++ src/app.ts
@@ -13,4 +13,4 @@ app.use(express.json());
 app.use(handleApplicationErrors);
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Servidor is up on: ${port}`));
+app.listen(port, () => console.log(`Server is up on port: ${port}`));
