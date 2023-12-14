467b7403ef21570688aaff735bd5e256f10003d1
yantavares <yantdo1@gmail.com>
2023-12-14 15:27:59 -0300

    Refactor loan-router and loan-repository

--- src/routes/loan-router.ts
+++ src/routes/loan-router.ts
@@ -1,15 +1,13 @@
 import { Router } from "express";
-import { validateSchema } from "../middlewares/validateSchema.js";
 import loanControllers from "../controllers/loan-controllers.js";
-import { renewLoanSchema } from "../schemas/loan-schemas.js";
 
-const loanRouter = Router()
+const loanRouter = Router();
 
-loanRouter.post("/loan/:itemId", loanControllers.createLoan)
-loanRouter.delete("/loan/:itemId", loanControllers.completeLoan)
-loanRouter.put("/loan/:itemId", validateSchema(renewLoanSchema), loanControllers.renewLoan)
-loanRouter.get("/loan", loanControllers.listLoans)
-loanRouter.get("/loan/book", loanControllers.listBookLoans)
-loanRouter.get("/loan/material", loanControllers.listMaterialLoans)
+loanRouter.post("/loan/:itemId", loanControllers.createLoan);
+loanRouter.delete("/loan/:itemId", loanControllers.completeLoan);
+loanRouter.put("/loan/:itemId", loanControllers.renewLoan);
+loanRouter.get("/loan", loanControllers.listLoans);
+loanRouter.get("/loan/book", loanControllers.listBookLoans);
+loanRouter.get("/loan/material", loanControllers.listMaterialLoans);
 
 export default loanRouter;
--- src/services/loan-services.ts
+++ src/services/loan-services.ts
@@ -4,122 +4,136 @@ import materialRepository from "../repositories/material-repository.js";
 import { Usuario } from "../types/user-types.js";
 
 async function registerLoan(itemId: any, usuario: Usuario) {
-    let item = await loanRepository.findItemByIsbn(itemId);
+  let item = await loanRepository.findItemByIsbn(itemId);
+  if (item.rowCount === 0) {
+    item = await loanRepository.findItemByMaterialId(itemId);
     if (item.rowCount === 0) {
-        item = await loanRepository.findItemByMaterialId(itemId);
-        if (item.rowCount === 0) {
-            throw errors.notFoundError();
-        }
+      throw errors.notFoundError();
     }
-    itemId = item.rows[0].id;
+  }
+  itemId = item.rows[0].id;
 
-    const userAlreadyLoanedThisItem = await loanRepository.findLoanByItemIdAndUserId(itemId, usuario.id)
-    if(userAlreadyLoanedThisItem.rowCount !== 0) throw errors.loanInProgressError()
+  const userAlreadyLoanedThisItem =
+    await loanRepository.findLoanByItemIdAndUserId(itemId, usuario.id);
+  if (userAlreadyLoanedThisItem.rowCount !== 0)
+    throw errors.loanInProgressError();
 
-    const data_emprestimo = new Date()
-    const data_devolucao = new Date(data_emprestimo);
-    data_devolucao.setDate(data_devolucao.getDate() + 30);
-    const loan = {itemId, data_emprestimo, data_devolucao, status: true}
+  const data_emprestimo = new Date();
+  const data_devolucao = new Date(data_emprestimo);
+  data_devolucao.setDate(data_devolucao.getDate() + 30);
+  const loan = { itemId, data_emprestimo, data_devolucao, status: true };
 
-    const itemExists = await loanRepository.findItemById(itemId)
-    if(itemExists.rowCount === 0 ) throw errors.notFoundError()
-    const loans = await loanRepository.findLoansByItemId(itemId)
+  const itemExists = await loanRepository.findItemById(itemId);
+  if (itemExists.rowCount === 0) throw errors.notFoundError();
+  const loans = await loanRepository.findLoansByItemId(itemId);
 
-    if(item.rows[0].isbn) {
-        console.log('alugando livro')
-        const bookExists = await materialRepository.findBookByISBN(item.rows[0].isbn)
-        if(bookExists.rowCount === 0) throw errors.notFoundError()
-        if(loans.rowCount >= bookExists.rows[0].quantidade) throw errors.notEnoughItemsError()
-    } else if (item.rows[0].id_material) {
-        console.log('alugando material')
-        const materialExists = await materialRepository.findMaterialById(item.rows[0].id_material)
-        if(materialExists.rowCount === 0) throw errors.notFoundError()
-        if(loans.rowCount >= materialExists.rows[0].quantidade) throw errors.notEnoughItemsError()
-    }
+  if (item.rows[0].isbn) {
+    console.log("alugando livro");
+    const bookExists = await materialRepository.findBookByISBN(
+      item.rows[0].isbn
+    );
+    if (bookExists.rowCount === 0) throw errors.notFoundError();
+    if (loans.rowCount >= bookExists.rows[0].quantidade)
+      throw errors.notEnoughItemsError();
+  } else if (item.rows[0].id_material) {
+    console.log("alugando material");
+    const materialExists = await materialRepository.findMaterialById(
+      item.rows[0].id_material
+    );
+    if (materialExists.rowCount === 0) throw errors.notFoundError();
+    if (loans.rowCount >= materialExists.rows[0].quantidade)
+      throw errors.notEnoughItemsError();
+  }
 
-    await loanRepository.insertLoan(usuario.id, loan)
+  await loanRepository.insertLoan(usuario.id, loan);
 }
 
 async function completeLoan(itemId: any, usuario: Usuario) {
-    let item = await loanRepository.findItemByIsbn(itemId);
+  let item = await loanRepository.findItemByIsbn(itemId);
+  if (item.rowCount === 0) {
+    item = await loanRepository.findItemByMaterialId(itemId);
     if (item.rowCount === 0) {
-        item = await loanRepository.findItemByMaterialId(itemId);
-        if (item.rowCount === 0) {
-            throw errors.notFoundError();
-        }
+      throw errors.notFoundError();
     }
-    itemId = item.rows[0].id;
+  }
+  itemId = item.rows[0].id;
 
-    const userLoanedThisItem = await loanRepository.findLoanByItemIdAndUserId(itemId, usuario.id)
-    if(userLoanedThisItem.rowCount === 0) throw errors.notFoundError()
+  const userLoanedThisItem = await loanRepository.findLoanByItemIdAndUserId(
+    itemId,
+    usuario.id
+  );
+  if (userLoanedThisItem.rowCount === 0) throw errors.notFoundError();
 
-    const today = new Date()
-    const dataDevolucao = new Date(userLoanedThisItem.rows[0].data_devolucao);
-    const timeDiff = (dataDevolucao.getTime() - today.getTime());
-    const differenceInDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-    if(differenceInDays < 0) throw errors.pendingFineError()
+  const today = new Date();
+  const dataDevolucao = new Date(userLoanedThisItem.rows[0].data_devolucao);
+  const timeDiff = dataDevolucao.getTime() - today.getTime();
+  const differenceInDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+  if (differenceInDays < 0) throw errors.pendingFineError();
 
-    await loanRepository.deleteLoanByItemIdAndUserId(itemId, usuario.id)
+  await loanRepository.deleteLoanByItemIdAndUserId(itemId, usuario.id);
 }
 
-async function renewLoan(itemId: any, usuario: Usuario, newDate: any) {
-    let item = await loanRepository.findItemByIsbn(itemId);
+async function renewLoan(itemId: any, usuario: Usuario) {
+  let item = await loanRepository.findItemByIsbn(itemId);
+  if (item.rowCount === 0) {
+    item = await loanRepository.findItemByMaterialId(itemId);
     if (item.rowCount === 0) {
-        item = await loanRepository.findItemByMaterialId(itemId);
-        if (item.rowCount === 0) {
-            throw errors.notFoundError();
-        }
+      throw errors.notFoundError();
     }
-    itemId = item.rows[0].id;
-    
-    const userLoanedThisItem = await loanRepository.findLoanByItemIdAndUserId(itemId, usuario.id)
-    if(userLoanedThisItem.rowCount === 0) throw errors.notFoundError()
+  }
+  itemId = item.rows[0].id;
 
-    const today = new Date()
-    const dataDevolucao = new Date(userLoanedThisItem.rows[0].data_devolucao);
-    const timeDiff = (dataDevolucao.getTime() - today.getTime());
-    const differenceInDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-    if(differenceInDays < 0) throw errors.pendingFineError()
+  const userLoanedThisItem = await loanRepository.findLoanByItemIdAndUserId(
+    itemId,
+    usuario.id
+  );
+  if (userLoanedThisItem.rowCount === 0) throw errors.notFoundError();
 
-    await loanRepository.updateReturnDate(itemId, usuario.id, newDate)
+  const today = new Date();
+  const dataDevolucao = new Date(userLoanedThisItem.rows[0].data_devolucao);
+  const timeDiff = dataDevolucao.getTime() - today.getTime();
+  const differenceInDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+  if (differenceInDays < 0) throw errors.pendingFineError();
+
+  dataDevolucao.setDate(today.getDate() + 20); // set the new date to 20 days from today
+
+  await loanRepository.updateReturnDate(itemId, usuario.id, dataDevolucao);
 }
-
 async function listLoans(userId: number) {
-    const loans = await loanRepository.listUserLoans(userId)
-    const loansData: any[] = [];
+  const loans = await loanRepository.listUserLoans(userId);
+  const loansData: any[] = [];
 
-    for (const i of loans.rows) {
-        if (i.isbn === null) {
-            console.log('material')
-            console.log(i)
-            const loan = await loanRepository.listMaterialInfoByItemId(i.item_id);
-            loansData.push(loan.rows[0]);
-        } else {
-            console.log('book')
-            console.log(i)
-            const loan = await loanRepository.listBookInfoByItemId(i.item_id);
-            loansData.push(loan.rows[0]);
-        }
+  for (const i of loans.rows) {
+    if (i.isbn === null) {
+      console.log("material");
+      console.log(i);
+      const loan = await loanRepository.listMaterialInfoByItemId(i.item_id);
+      loansData.push(loan.rows[0]);
+    } else {
+      console.log("book");
+      console.log(i);
+      const loan = await loanRepository.listBookInfoByItemId(i.item_id);
+      loansData.push(loan.rows[0]);
     }
-    return loansData
+  }
+  return loansData;
 }
 
 async function listBookLoans() {
-    const loans = await loanRepository.listBookLoans()
-    return loans.rows
+  const loans = await loanRepository.listBookLoans();
+  return loans.rows;
 }
 
 async function listMaterialLoans() {
-    const loans = await loanRepository.listMaterialLoans()
-    return loans.rows
+  const loans = await loanRepository.listMaterialLoans();
+  return loans.rows;
 }
-
 
 export default {
-    registerLoan,
-    completeLoan,
-    renewLoan,
-    listLoans,
-    listBookLoans,
-    listMaterialLoans
-}
+  registerLoan,
+  completeLoan,
+  renewLoan,
+  listLoans,
+  listBookLoans,
+  listMaterialLoans,
+};
